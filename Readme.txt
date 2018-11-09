6022fef27c54de15c1adf4c33ddff679893fb94b
Eshana <eshana95@gmail.com>
2018-11-09 15:30:15 +0530

    App Fix

---
+++ Readme.txt
@@ -0,0 +1,16 @@
+
+
+To Start Please run
+
+nodemon server.js
+
+
+Then Go to Browser
+
+on http://localhost:8000/
+
+Hit Enter
+
+
+Results will show on the Console
+
--- server.js
+++ server.js
@@ -6,31 +6,102 @@ const express = require('express');
 const app = express();
 
 
-// Import Keys and Variables
+var arraySort = require('array-sort');
 
 
-const keys = require('./config/variables');
+// Get JSON Out Put From File
 
-// Import Mongoose to Connect DB
-
-const mongoose = require('mongoose');
+const ocroutput = require('./ocroutput/output');
 
 
-// Connect to MongoDB
-
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true}).then(() => console.log('Mongo Connected')).catch(err => console.log(err));
+var ocr = ocroutput.output;
 
 
-app.get('/', (request,response) => {
+// New array Sorted JSON
+
+var sortedJSON = [];
 
 
-    //response.send("{State:Up}");
+
+app.get('/',async (request,response) => {
+
 
     response.json({State:"Up"})
 
-    console.log("Here");
+
+    // Push Details to Array
+
+
+    for(var i = 0 ; i < ocr.length ; i++){
+
+
+       // Finding Center X
+
+        var centerx = ocr[i].boundaries[0].x + ocr[i].boundaries[1].x + ocr[i].boundaries[2].x
+                     + ocr[i].boundaries[3].x / 4
+
+
+        // Finding Center Y
+
+        var centery = ocr[i].boundaries[0].y + ocr[i].boundaries[1].y + ocr[i].boundaries[2].y
+            + ocr[i].boundaries[3].y / 4
+
+
+        // Create Object to Push New Array
+
+        var object = {
+            description: ocr[i].description ,
+            x:centerx,
+            y:centery,
+
+            }
+
+        sortedJSON.push(object);
+
+    }
+
+
+    // Sorting Array
+
+    await sortedJSON.sort(function(value_one, value_two) {
+
+        return parseFloat(value_one.y) - parseFloat(value_two.y) || parseFloat(value_one.x) - parseFloat(value_two.x) ;
+    });
+
+
+    for (var i = 0; i < sortedJSON.length; i++) {
+
+        var previous = i - 1;
+
+        if (i != 0) {
+
+            var basey = sortedJSON[previous].y;
+
+        } else {
+
+            var basey = sortedJSON[i].y;
+        }
+
+
+        if (basey == sortedJSON[i].y) {
+
+            process.stdout.write(' ');
+            process.stdout.write(sortedJSON[i].description);
+
+        } else {
+
+            process.stdout.write('\n');
+            process.stdout.write(sortedJSON[i].description);
+
+        }
+
+    }
+
+
+    console.log(sortedJSON);
 
 });
 
 
-app.listen(8000, ()=> console.log('Start'));
+app.listen(8000, ()=> console.log('Start Application'));
+
